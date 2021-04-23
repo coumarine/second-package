e41b9782f34cab3b19c7c03bc835278a104cbc17
SMK <coumarine@gmail.com>
2021-04-23 10:45:02 +0100

    Update commit email

--- src/main/java/secondcheck/fortest/testSecondPackage.java
+++ src/main/java/secondcheck/fortest/testSecondPackage.java
@@ -3,5 +3,6 @@ package secondcheck.fortest;
 public class testSecondPackage {
     public static void secondDisplay(String param){
         System.out.println("this is second page" + param);
+
     }
 }
